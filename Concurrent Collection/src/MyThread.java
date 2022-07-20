2e479f0321de642ef6091999948198b64b2347d1
kaviarasukumarasamy <107115293+kaviarasukumarasamy@users.noreply.github.com>
2022-07-19 17:02:29 -0700

    commit

---
+++ Concurrent Collection/src/MyThread.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class MyThread {
+
+	public static void main(String[] args) {
+		List<Integer> list = new CopyOnWriteArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+
+		Iterator<Integer> it = list.iterator();
+		while (it.hasNext()) {
+			Integer value = it.next();
+			System.out.println("List Value:" + value);
+			if (value.equals(3))
+				list.remove(value);
+			System.out.println("" + list);
+		}
+
+	}
+}
